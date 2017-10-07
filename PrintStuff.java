7813e637b4086a0c91fc5d850d338ea7c723dc2e
dmcoomes <damiancoomes@gmail.com>
2017-10-07 11:25:30 -0500

    Damian's practice commit

--- PrintStuff.java
+++ PrintStuff.java
@@ -2,5 +2,6 @@ public class PrintStuff {
 	public static void main(String[] args) {
 		System.out.println("Make this script do something!");
 		System.out.println("Brandon");
+                System.out.println("Damian");
 	}
 }
