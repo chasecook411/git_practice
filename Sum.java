f83081159bbfe81c3b016b359e8cb1a4cecd6478
dmcoomes <damiancoomes@gmail.com>
2017-10-08 17:47:54 -0500

    Summation loop

---
+++ Sum.java
@@ -0,0 +1,13 @@
+public class Sum
+{
+  public static void main(String args[])
+  {
+    int n = 100;
+    int sum = 0;
+    for(int i = 0; i < n; i++)
+    {
+      sum += i;
+    }
+    System.out.println("Sum: " + sum);
+  }
+}
